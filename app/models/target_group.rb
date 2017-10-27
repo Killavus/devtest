d4305466a962e196a5982ee7ed1e4600e401e5da
Marcin Grzywaczewski <killavus@gmail.com>
2017-10-27 06:02:02 +0200

    Fixed validation which would never pass on create - external_id is not loaded automatically by Rails

--- app/models/target_group.rb
+++ app/models/target_group.rb
@@ -2,5 +2,5 @@ class TargetGroup < ActiveRecord::Base
   belongs_to :parent, class_name: 'TargetGroup'
   belongs_to :panel_provider
 
-  validates :panel_provider, :name, :external_id, :secret_code, presence: true
+  validates :panel_provider, :name, :secret_code, presence: true
 end
