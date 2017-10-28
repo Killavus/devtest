e331c7db35574690f0eca375fe51f4cc2c492693
Marcin Grzywaczewski <killavus@gmail.com>
2017-10-28 17:37:35 +0200

    Implemented public API for target groups

--- app/presenters/target_group_presenter.rb
+++ app/presenters/target_group_presenter.rb
@@ -1,22 +1,20 @@
 class TargetGroupPresenter
-  class TargetGroupPresenter
-    def as_json(collection)
-      collection.map(&method(:hierarchy_as_json))
-    end
+  def as_json(collection)
+    collection.map(&method(:hierarchy_as_json))
+  end
 
-    private
+  private
 
-    def hierarchy_as_json(hierarchy)
-      {
-        country_ids: hierarchy.countries.map(&:id),
-        nodes: hierarchy.map do |node|
-          {
-            name: node.name,
-            id: node.__ar.external_id,
-            parent_id: node.parent.try(:__ar).try(:external_id)
-          }
-        end
-      }
-    end
+  def hierarchy_as_json(hierarchy)
+    {
+      country_ids: hierarchy.countries.map(&:id),
+      nodes: hierarchy.map do |node|
+        {
+          name: node.name,
+          id: node.__ar.external_id,
+          parent_id: node.parent.try(:__ar).try(:external_id)
+        }
+      end
+    }
   end
 end
